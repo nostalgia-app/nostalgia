b53463e5d180244b862c9c4e84924b139d6cc65f
Scott L <ehee27@gmail.com>
2023-03-08 17:33:59 -0600

    user model and route

--- script/seed.js
+++ script/seed.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
 const {
   db,
   models: { User, Community, User_Community },
-} = require("../server/db");
+} = require('../server/db');
 
 /**
  * seed - this function clears the database, updates tables to
@@ -11,47 +11,41 @@ const {
  */
 async function seed() {
   await db.sync({ force: true }); // clears db and matches models to tables
-  console.log("db synced!");
-
-  // Creating Users
-  const users = await Promise.all([
-    User.create({ username: "cody", password: "123" }),
-    User.create({ username: "murphy", password: "123" }),
-  ]);
+  console.log('db synced!');
 
   // Creating Communities
   const communities = await Promise.all([
     Community.create({
-      name: "West Broadway Neighborhood Association",
-      bio: "West Broadway Neighborhood Association organizes neighbors and businesses on the West Side of Providence to preserve and promote our diverse, historic, urban community as a safe, vibrant, and sustainable place to be SWELL (Shop, Work, Eat, Live, and Learn locally).",
-      address: "1560 Westminster St",
-      city: "Providence",
-      state: "RI",
-      zipCode: "02909",
+      name: 'West Broadway Neighborhood Association',
+      bio: 'West Broadway Neighborhood Association organizes neighbors and businesses on the West Side of Providence to preserve and promote our diverse, historic, urban community as a safe, vibrant, and sustainable place to be SWELL (Shop, Work, Eat, Live, and Learn locally).',
+      address: '1560 Westminster St',
+      city: 'Providence',
+      state: 'RI',
+      zipCode: '02909',
       imageUrl:
-        "https://images.squarespace-cdn.com/content/v1/547486a6e4b081cfb2fbb049/1653424790873-H5ZT92BX336T2MRPK0JH/277988277_7237088149698005_7506566997549425627_n.jpg?format=1500w",
+        'https://images.squarespace-cdn.com/content/v1/547486a6e4b081cfb2fbb049/1653424790873-H5ZT92BX336T2MRPK0JH/277988277_7237088149698005_7506566997549425627_n.jpg?format=1500w',
       adminId: users[0].id,
     }),
     Community.create({
-      name: "The Addams Family",
-      bio: "The Simpsons are a nuclear family consisting of married couple Homer and Marge and their three children, Bart, Lisa, and Maggie",
-      address: "21 Chester Place",
-      city: "Los Angeles",
-      state: "CA",
-      zipCode: "90007",
+      name: 'The Addams Family',
+      bio: 'The Simpsons are a nuclear family consisting of married couple Homer and Marge and their three children, Bart, Lisa, and Maggie',
+      address: '21 Chester Place',
+      city: 'Los Angeles',
+      state: 'CA',
+      zipCode: '90007',
       imageUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Addams_Family_main_cast_1964.JPG/640px-Addams_Family_main_cast_1964.JPG",
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Addams_Family_main_cast_1964.JPG/640px-Addams_Family_main_cast_1964.JPG',
       adminId: users[1].id,
     }),
     Community.create({
-      name: "University of Texas, Class of 2021",
-      bio: "Alumni from class of 2021",
-      address: "110 Inner Campus Drive",
-      city: "Austin",
-      state: "TX",
-      zipCode: "78705",
+      name: 'University of Texas, Class of 2021',
+      bio: 'Alumni from class of 2021',
+      address: '110 Inner Campus Drive',
+      city: 'Austin',
+      state: 'TX',
+      zipCode: '78705',
       imageUrl:
-        "https://news.utexas.edu/wp-content/uploads/2021/05/Senior_Stories_2021_Header_1500x543_SB-600x0-c-default.gif",
+        'https://news.utexas.edu/wp-content/uploads/2021/05/Senior_Stories_2021_Header_1500x543_SB-600x0-c-default.gif',
       adminId: users[1].id,
     }),
   ]);
@@ -84,6 +78,32 @@ async function seed() {
   console.log(`seeded ${communities.length} communities`);
   console.log(`seeded successfully`);
 
+  console.log('db synced!');
+
+  // Creating Users
+  const users = await Promise.all([
+    User.create({
+      username: 'cody',
+      password: '123',
+      firstName: 'Cody',
+      lastName: 'Code',
+      age: 27,
+      location: 'New York, NY',
+      bio: 'Hey man, my name is Cody.',
+    }),
+    User.create({
+      username: 'murphy',
+      password: '123',
+      firstName: 'Murphy',
+      lastName: 'McMurphy',
+      age: 98,
+      location: 'Dublin, Ireland',
+      bio: "I'm looking for a guy named Cody. Is he here?",
+    }),
+  ]);
+
+  console.log(`seeded ${users.length} users`);
+  console.log(`seeded successfully`);
   return {
     users: {
       cody: users[0],
@@ -103,16 +123,16 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log("seeding...");
+  console.log('seeding...');
   try {
     await seed();
   } catch (err) {
     console.error(err);
     process.exitCode = 1;
   } finally {
-    console.log("closing db connection");
+    console.log('closing db connection');
     await db.close();
-    console.log("db connection closed");
+    console.log('db connection closed');
   }
 }
 
