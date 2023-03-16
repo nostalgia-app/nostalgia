9ec885c5d2480c187ba670525517da6469f69f5f
Gary White <glw494@gmail.com>
2023-03-15 20:41:05 -0400

    Deleted image reducer and thunks

--- client/store/index.js
+++ client/store/index.js
@@ -3,7 +3,6 @@ import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import auth from './auth';
 import user from './user';
-import image from './image';
 import communities from './communities';
 import community from './community';
 import geographies from './geographies';
@@ -16,19 +15,19 @@ const reducer = combineReducers({
   community,
   geographies,
   user,
-  image,
 });
+
 const middleware = applyMiddleware(
   thunkMiddleware,
   createLogger({ collapsed: true })
 );
+
 const store = createStore(reducer, middleware);
 
 export default store;
 
 export * from './auth';
 export * from './user';
-export * from './image';
 export * from './communities';
 export * from './community';
 export * from './geographies';
