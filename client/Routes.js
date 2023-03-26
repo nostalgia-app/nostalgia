17adf1ed7dc4bdc2c5a66788f56257b5c84c748f
michun10 <michelleun@gmail.com>
2023-03-26 18:56:13 -0400

    fixing deploy bug

--- client/Routes.js
+++ client/Routes.js
@@ -1,19 +1,19 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
-import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { Login, Signup } from './components/users/Login';
-import { Home } from './components/Home';
-import { me } from './store';
-import CommunityList from './components/communities/CommunityList';
-import CommunityDetails from './components/communities/CommunityDetails';
-import CreateUser from './components/users/CreateUSer';
-import UpdateUser from './components/users/UpdateUser';
-import UserProfile from './components/users/UserProfile';
-import Success from './components/users/Success';
-import ArtifactList from './components/artifacts/ArtifactList';
-import ArtifactDetails from './components/artifacts/ArtifactDetails';
-import FriendsList from './components/friends/FriendsList';
-import MyFriendsList from './components/friends/MyFriendsList';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route } from "react-router-dom";
+import { Login } from "./components/users/Login";
+import { Home } from "./components/Home";
+import { me } from "./store";
+import CommunityList from "./components/communities/CommunityList";
+import CommunityDetails from "./components/communities/CommunityDetails";
+import CreateUser from "./components/users/CreateUser";
+import UpdateUser from "./components/users/UpdateUser";
+import UserProfile from "./components/users/UserProfile";
+import Success from "./components/users/Success";
+import ArtifactList from "./components/artifacts/ArtifactList";
+import ArtifactDetails from "./components/artifacts/ArtifactDetails";
+import FriendsList from "./components/friends/FriendsList";
+import MyFriendsList from "./components/friends/MyFriendsList";
 
 /**
  * COMPONENT
@@ -67,7 +67,7 @@ class Routes extends Component {
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
@@ -75,7 +75,7 @@ const mapState = state => {
   };
 };
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     loadInitialData() {
       dispatch(me());
--- client/components/users/CreateUser.js
+++ client/components/users/CreateUser.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { createNewUser, fetchUsers } from '../../store';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createNewUser } from "../../store";
+import { useHistory } from "react-router-dom";
 
 import {
   Container,
@@ -9,18 +9,18 @@ import {
   Typography,
   Grid,
   makeStyles,
-} from '@material-ui/core';
-import TextField from '@mui/material/TextField';
+} from "@material-ui/core";
+import TextField from "@mui/material/TextField";
 
 const useStyles = makeStyles({
   container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   form: {
-    display: 'flex',
-    flexDirection: 'column',
+    display: "flex",
+    flexDirection: "column",
   },
 });
 
@@ -29,12 +29,12 @@ const CreateUser = () => {
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(
       createNewUser({
@@ -44,7 +44,7 @@ const CreateUser = () => {
         password: password,
       })
     );
-    history.push('/login');
+    history.push("/login");
   };
 
   return (
@@ -56,25 +56,25 @@ const CreateUser = () => {
         <Grid item xs={12} sm={12} md={12}>
           <form className={classes.form} onSubmit={handleSubmit}>
             <TextField
-              onChange={e => setFirstName(e.target.value)}
+              onChange={(e) => setFirstName(e.target.value)}
               label="First Name"
               margin="normal"
               variant="outlined"
             />
             <TextField
-              onChange={e => setLastName(e.target.value)}
+              onChange={(e) => setLastName(e.target.value)}
               label="Last Name"
               margin="normal"
               variant="outlined"
             />
             <TextField
-              onChange={e => setUserName(e.target.value)}
+              onChange={(e) => setUserName(e.target.value)}
               label="Username"
               margin="normal"
               variant="outlined"
             />
             <TextField
-              onChange={e => setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
               label="Password"
               margin="normal"
               variant="outlined"
